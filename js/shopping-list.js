5ef0da04cc5138b445320cb532ba9bec7ba98634
chadgmoore <smap2d@gmail.com>
2015-02-05 17:14:15 -0500

    got all functionality back, added some animation to moving on the list via .slideDown

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,38 +1,30 @@
 $(document).ready(function() {
 
-	$('ul').on('click', 'li:nth-child(odd)', function(event){
-  		$(this).toggleClass('checked');
-  		$('ul').append(this)
-  		$(this).css('background-color','#2B3244')
-  		$(this).css('color','#fff')
-  		$(this).css('font-style', 'italic')  		
-
-})
-
-	$('ul').on('click', 'li:nth-child(even)', function(event){
-  		$(this).toggleClass('checked');
-  		$('ul').append(this)
-  		$(this).css('background-color','#2B3244')
-  		$(this).css('color','#fff') 
-  		$(this).css('font-style', 'italic')
-
-})
-
-
+  //Checkoff
+  $('#list').on('click', 'li.listitem', function(event){
+    $(this).slideUp('slow', function(){
+        $(this).slideDown('slow').appendTo('#list');
+        $(this).css('background-color', '#2B3244');
+        $(this).css('color', '#4C555A')
+        $('.item').css('color', "#4C555A");
+      })
+  })
+    
+  //add item
   $('.input').keypress(function(e) {
     if(e.which == 13) {
       e.preventDefault();
       var newItem = document.getElementsByName('newItem')[0];
-      $('.listUl').prepend('<li id="listId">' + newItem.value + '<button class="deleteButton"> <i class="fa fa-close fa-1x"></i></button></li>');
-      // set the text entry box back to blank
-      // e.defaultValue();
+      // $('.listUl').prepend('<li id="listId">' + newItem.value + '<button class="deleteButton"> <i class="fa fa-close fa-1x"></i></button></li>');
+      $('#list').prepend('<li class="listitem"><span class="item">' + newItem.value + '</span><div class="delete"><i class="fa fa-close fa-3x"></i></div></li>').slideDown('slow');
       document.getElementById("itemEntry").reset();
     }
 })
 
-
-  $(".deleteButton").click(function(){
-    $(this).parent('li id="listId"').remove();    
+  //delete item
+  $(".delete").click(function(){
+    $(this).parent('li').remove();
   })
 
 });
+
